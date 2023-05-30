66e267a593fd0bf9697d7db40d04f7c107b8b38e
gegete <gegete@qq.com>
2023-05-30 10:25:54 +0000

    xout

--- run_env.sh
+++ run_env.sh
@@ -1,8 +1,8 @@
-# chmod 777 /content/drive/MyDrive/Anaconda3-2020.02-Linux-x86_64.sh
-# ./content/drive/MyDrive/Anaconda3-2020.02-Linux-x86_64.sh
-# conda env create -f environment.yaml
-# source ~/.bashrc
-# conda activate ldm
+chmod 777 /content/drive/MyDrive/Anaconda3-2020.02-Linux-x86_64.sh
+./content/drive/MyDrive/Anaconda3-2020.02-Linux-x86_64.sh
+conda env create -f environment.yaml
+source ~/.bashrc
+conda activate ldm
 cd ..
 pwd
 git clone https://github.com/facebookresearch/xformers.git
@@ -12,3 +12,4 @@ pip install -r requirements.txt
 pip install -e .
 cd ../stablediffusion
 wget https://huggingface.co/stabilityai/stable-diffusion-2/resolve/main/768-v-ema.ckpt
+wget https://huggingface.co/stabilityai/stable-diffusion-2-base/resolve/main/512-base-ema.ckpt
--- run_exe.sh
+++ run_exe.sh
@@ -1,4 +1,9 @@
+# python ./scripts/txt2img.py --prompt "dog" \
+#     --ckpt  checkpoints/768-v-ema.ckpt\
+#     --config configs/stable-diffusion/v2-inference-v.ymal\
+#     --H 128 --W 128 --n_iter 50 --step 1 --device "cuda"
+
 python ./scripts/txt2img.py --prompt "dog" \
-    --ckpt  checkpoints/768-v-ema.ckpt\
-    --config configs/stable-diffusion/v2-inference-v.ymal\
-    --H 128 --W 128 --n_iter 1 --step 1 --device "cuda"
+    --ckpt  checkpoints/512-base-ema.ckpt\
+    --config configs/stable-diffusion/v2-inference.ymal\
+    --H 512 --W 512 --n_iter 50 --step 1 --device "cuda"
