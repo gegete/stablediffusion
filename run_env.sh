2d10c20f1880965a7ba3c76a394b7d79be7a8c39
yn.zhang <yn.zhang@shenjugroup.com>
2023-05-30 17:32:57 +0800

    add exe env_x

--- run_env.sh
+++ run_env.sh
@@ -11,3 +11,4 @@ git submodule update --init --recursive
 pip install -r requirements.txt
 pip install -e .
 cd ../stablediffusion
+wget https://huggingface.co/stabilityai/stable-diffusion-2/resolve/main/768-v-ema.ckpt
